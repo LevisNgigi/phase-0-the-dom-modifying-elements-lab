a96444f9bf7a42f379164e2411a4a70e84cdc701
Levis <Ngigilevis@gmail.com>
2024-01-01 21:50:28 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Test 1
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Test 2
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+//Test 3
+newHeader.innerHTML = 'YOUR-NAME is the champion';
